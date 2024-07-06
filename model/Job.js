88d4f8f22bc88e0f6ad697c20d5755d50077433a
Nick Nguyen <khangnick14@gmail.com>
2024-07-06 22:41:05 +0700

    define schema + add two entity classes

---
+++ model/Job.js
@@ -0,0 +1,34 @@
+import { sequelize } from "../server";
+import { DataTypes } from "sequelize";
+import User from "./User";
+
+const Job = sequelize.define(
+  "Job",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User,
+        key: "id",
+      },
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: false,
+  }
+);
+
+User.hasMany(Job, { foreignKey: "user_id", onDelete: "CASCADE" });
+Job.belongsTo(User, { foreignKey: "user_id" });
+
+export default Job;
---
+++ model/User.js
@@ -0,0 +1,29 @@
+import { sequelize } from "../server";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: false,
+  }
+);
+
+export default User;
